33f3365c9d3042d4f6358a9fa22bf44cda62de0a
Laurent Peuch <cortex@worlddomination.be>
2016-04-27 07:37:13 +0200

    [enh] use detailcomponent in detailpageview

--- building_blocks/page_views.py
+++ building_blocks/page_views.py
@@ -1,5 +1,7 @@
 from django.views.generic import DetailView
 
+from .components import DetailComponent
+
 
 class DetailPageView(DetailView):
     def get_template_names(self):
@@ -7,3 +9,9 @@ class DetailPageView(DetailView):
             "pages/detail.html",
             "pages/%s_detail.html" % self.object._meta.model_name,
         ]
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(DetailPageView, self).get_context_data(*args, **kwargs)
+        context["component"] = DetailComponent(self.object)
+
+        return context
